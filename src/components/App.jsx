5a18bc4afee65b8f5d71ee67b3e18c74d7b2c887
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-07-30 20:25:37 +0300

    hook refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,61 @@ import { Section } from './section/section';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './contactForm/contactForm';
 import { ContactList } from './contactList/contactList';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Filter } from './filter/filter';
 
-export class App extends Component {
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (name, number) => {
+    const id = nanoid();
+    const newContact = { name, number, id };
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const handleDelete = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <Section title={'Phonebook'} />
+      <ContactForm addContact={addContact} contacts={contacts} />
+
+      <Section title={'Contacts'} />
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} handleDelete={handleDelete} />
+    </div>
+  );
+};
+/*
+export class ClassApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -70,3 +121,4 @@ export class App extends Component {
     );
   }
 }
+*/
